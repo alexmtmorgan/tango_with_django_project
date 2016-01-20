fc34728006e691802b4c1b4ee1f00160d53b99b3
alexmtmorgan <alexmtmorgan@gmail.com>
2016-01-20 14:18:33 +0000

    updated urls.py to route /rango urls to rango urls.py file

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'tango_with_django_project.views.home', name='home'),
+    # url(r'^$', 'tango_with_django_project_17.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^rango/', include('rango.urls')),
 )
