54377df67020177aef1f01b06647343e5e71223b
alexmtmorgan <alexmtmorgan@gmail.com>
2016-01-20 14:28:17 +0000

    added new route /about

--- rango/views.py
+++ rango/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Rango says hey there world!")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page.")
