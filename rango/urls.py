5848cbb547f33c08993302271e75274836c31be6
alexmtmorgan <alexmtmorgan@gmail.com>
2016-01-20 14:35:24 +0000

    fixed strings to display when loading index and about pages

--- rango/urls.py
+++ rango/urls.py
@@ -3,4 +3,4 @@ from rango import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
-                       url(r'^about$', views.about, name='about'))
+                       url(r'^about/$', views.about, name='about'))
--- rango/views.py
+++ rango/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("Rango says hey there world!")
+    return HttpResponse("Rango says: Hello world! <br/> <a href='/rango/about'>About</a>")
+
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.")
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
