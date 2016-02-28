492fa67156d7d7a841530485fd32af3886e015fc
alexmtmorgan <alexmtmorgan@gmail.com>
2016-02-28 13:24:52 +0000

    finished up to and including chapter 12

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -38,8 +38,6 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-LOGIN_URL = '/rango/login/'
-
 PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
@@ -53,7 +51,15 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration',
 )
+
+REGISTRATION_OPEN = True                # If True, users can register
+ACCOUNT_ACTIVATION_DAYS = 7     # One-week activation window; you may, of course, use a different value.
+REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
+LOGIN_REDIRECT_URL = '/rango/'  # The page you want users to arrive at after they successfully log in
+LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in,
+#  and are trying to access pages requiring authentication
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -4,19 +4,28 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from registration.backends.simple.views import RegistrationView
+
+
+# Create a new class that redirects the user to the index page, if successful at logging
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, request, user):
+        return '/rango/'
+
 urlpatterns = patterns('',
                        # Examples:
                        # url(r'^$', 'tango_with_django_project_17.views.home', name='home'),
                        # url(r'^blog/', include('blog.urls')),
-
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^rango/', include('rango.urls')),
+                       url(r'^accounts/', include('registration.backends.simple.urls')),
+                       url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
                        )
 
 # UNDERNEATH your urlpatterns definition, add the following two lines:
 if settings.DEBUG:
     urlpatterns += patterns(
-            'django.views.static',
-            (r'^media/(?P<path>.*)',
-             'serve',
-             {'document_root': settings.MEDIA_ROOT}), )
+        'django.views.static',
+        (r'^media/(?P<path>.*)',
+         'serve',
+         {'document_root': settings.MEDIA_ROOT}), )
