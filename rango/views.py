c202810da53a009ce79b21897863d3065eafa8a1
alexmtmorgan <alexmtmorgan@gmail.com>
2016-02-28 12:41:11 +0000

    finished up to and including chapter 11

--- rango/views.py
+++ rango/views.py
@@ -18,6 +18,9 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 
+# Import datetime for cookie stuff
+from datetime import datetime
+
 
 def index(request):
     # Query the database for a list of ALL categories currently stored.
@@ -25,18 +28,46 @@ def index(request):
     # Retrieve the top 5 only - or all if less than 5.
     # Place the list in our context_dict dictionary which will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    page_list = Page.objects.order_by('-views')[:5]
 
-    top_five_pages = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
 
-    context_dict['pages'] = top_five_pages
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
+    reset_last_visit_time = False
 
-    # Render the response and send it back!
-    return render(request, 'rango/index.html', context_dict)
+    last_visit = request.session.get('last_visit')
+    if last_visit:
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+
+        if (datetime.now() - last_visit_time).seconds > 0:
+            # ...reassign the value of the cookie to +1 of what it was before...
+            visits = visits + 1
+            # ...and update the last visit cookie, too.
+            reset_last_visit_time = True
+    else:
+        # Cookie last_visit doesn't exist, so create it to the current date/time.
+        reset_last_visit_time = True
+
+    if reset_last_visit_time:
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+    context_dict['visits'] = visits
+
+    response = render(request,'rango/index.html', context_dict)
+
+    return response
 
 
 def about(request):
-    return render(request, 'rango/about.html')
+
+    if request.session.get('visits'):
+        count = request.session.get('visits')
+    else:
+        count = 0
+
+    return render(request, 'rango/about.html', {'visits': count})
 
 
 def category(request, category_name_slug):
@@ -223,7 +254,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html', {})
 
 
 # Use the login_required() decorator to ensure only those logged in can access the view.
